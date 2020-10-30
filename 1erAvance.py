b74340746575ef3c96da69e192896e0fcb6cda9c
diegolimon18 <65428903+diegolimon18@users.noreply.github.com>
2020-10-29 19:45:38 -0600

    Update 1erAvance.py

--- 1erAvance.py
+++ 1erAvance.py
@@ -18,3 +18,15 @@ sp['Tipo_Flor']=sp['Tipo_Flor'].replace(['Iris-versicolor','Iris-virginica','Iri
 data=sp.values
 X= data[:,0:-1]
 y=data[:,-1]
+
+emb= FactorAnalysis(n_components=2)
+X1t =emb.fit_transform(X,y)
+plt.scatter(X1t[:,0],X1t[:,-1],c=y)
+plt.title('Iris dataset FactorAnalysis')
+plt.show()
+
+emb= LinearDiscriminantAnalysis(n_components=2)
+X2t =emb.fit_transform(X,y)
+plt.scatter(X2t[:,0],X2t[:,-1],c=y)
+plt.title('Iris dataset LinearDiscriminant')
+plt.show()
